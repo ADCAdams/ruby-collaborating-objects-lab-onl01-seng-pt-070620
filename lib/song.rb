5e787191498330757f74b3caaf9296ed5f1d250a
Andy Adams <andrewdcadams@gmail.com>
2020-08-04 13:15:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song 
   
   @@all = []
@@ -11,8 +12,18 @@ class Song
   def self.all 
     @@all 
   end
-  
-  
+
+  def self.new_by_filename(file_name)
+    string_array = file_name.split(" - ")
+    new_genre = string_array[2].delete_suffix('.mp3')
+    new_name = string_array[1]
+    new_artist = string_array[0]
+    
+    new_song = Song.new(new_name)
+    new_song.artist = new_artist
+    new_song
+    #binding.pry
+  end
   
   
 end 
